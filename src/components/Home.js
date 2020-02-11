55270bd97eec50ac29f4e6a7b6fbecd8ca68e069
SEANHXE <790438599@qq.com>
2020-02-11 01:15:05 -0600

    Add main tabs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import '../styles/Home.css';
+import { Tabs, Button } from 'antd';
+const { TabPane } = Tabs;
 
 export function Home() {
-  return (<div>Home</div>);
+  const operations = <Button>Creat New Post</Button>;
+  return (
+    <Tabs tabBarExtraContent={operations} className="main-tabs">
+      <TabPane tab="Images Posts" key="1">
+        Content of tab 1
+      </TabPane>
+      <TabPane tab="Video Posts" key="2">
+        Content of tab 2
+      </TabPane>
+      <TabPane tab="Maps" key="3">
+        Content of tab 3
+      </TabPane>
+    </Tabs>
+  );
 };
+
+
+
