f1dd57003d550611ea3cb96c44062cb4d91620e2
SEANHXE <790438599@qq.com>
2020-02-11 02:42:36 -0600

    4.3 loading post

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../styles/Home.css';
 import { Tabs, Button } from 'antd';
-import { GEOLOCATION_OPTIONS, POSITION_KEY } from '../constants';
+import {API_ROOT, AUTH_HEADER, GEOLOCATION_OPTIONS, POSITION_KEY, TOKEN_KEY} from '../constants';
 const { TabPane } = Tabs;
 
 export class Home extends React.Component {
   state = {
     loadingGeolocation: false,
+    loadingPosts: false,
     errorMessage: null,
   }
 
@@ -37,6 +38,7 @@ export class Home extends React.Component {
     console.log(position);
     const {latitude, longitude} = position.coords;
     localStorage.setItem(POSITION_KEY, JSON.stringify({latitude, longitude}));
+    this.loadNearbyPost();
   }
 
   onGeolocationFailure = () => {
@@ -44,6 +46,41 @@ export class Home extends React.Component {
       loadingGeolocation: false,
       errorMessage: 'Failed to load geolocation',
     });
+  }
+
+  loadNearbyPost() {
+    this.setState({
+      loadingPosts: true,
+      errorMessage: null,
+    });
+
+    const   position = JSON.parse(localStorage.getItem(POSITION_KEY));
+    const  range = 20;
+    const token = localStorage.getItem(TOKEN_KEY);
+
+    fetch(`${API_ROOT}/search?lat=${position.latitude}&lon=${position.longitude}&range=${range}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `${AUTH_HEADER} ${token}`,
+      }
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Failed to load posts');
+    }).then((data) => {
+        console.log(data);
+        this.setState({
+          loadingPosts: false,
+          posts: data ? data : [],
+        })
+    }).catch((error) => {
+      this.setState({
+        loadingPosts: false,
+        errorMessage: error.message,
+      })
+    })
+
   }
 
   componentDidMount() {
