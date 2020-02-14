03e7701c0ad8a1243ee6edd5e77d9967bc152e1f
SEANHXE <790438599@qq.com>
2020-02-13 23:11:09 -0600

    6.1 AroundMap component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import '../styles/Home.css';
 import { Tabs, Spin, Row, Col } from 'antd';
 import { Gallery } from './Gallery';
 import { CreatePostButton } from './CreatePostButton'
+import {AroundMap} from "./AroundMap"
 import {
   API_ROOT,
   AUTH_HEADER,
@@ -173,7 +174,12 @@ export class Home extends React.Component {
             {this.getPost(POST_TYPE_VIDEO)}
           </TabPane>
           <TabPane tab="Maps" key="3">
-            Content of tab 3
+            <AroundMap
+              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3CEh9DXuyjozqptVB5LA-dN7MxWWkr9s&v=3.exp&libraries=geometry,drawing,places"
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `400px` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+            />
           </TabPane>
         </Tabs>
     );
