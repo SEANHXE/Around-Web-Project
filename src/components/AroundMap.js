308f62f81621dc0d34ca3f7c71bb760d1c48ca59
SEANHXE <790438599@qq.com>
2020-02-13 23:31:00 -0600

    6.2 InfoWindow & Toggle

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -1,14 +1,34 @@
 import React from "react";
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps"
 
 export class NormalAroundMap extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
+  onToggleOpen = () => {
+    this.setState((prevState) => ({
+      isOpen: !prevState.isOpen,
+    }));
+  }
+
   render() {
     return (
         <GoogleMap
             defaultZoom={8}
             defaultCenter={{ lat: -34.397, lng: 150.644 }}
         >
-          <Marker position={{ lat: -34.397, lng: 150.644 }} />
+          <Marker
+            position={{ lat: -34.397, lng: 150.644 }}
+            onClick={this.onToggleOpen}
+          >
+            {this.state.isOpen ?
+                <InfoWindow>
+                  <div>InfoWindow</div>
+                </InfoWindow>
+            :
+            null}
+          </Marker>
         </GoogleMap>
     );
   }
